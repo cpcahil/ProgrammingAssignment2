867dd04532b865fb195860356f2d125921a39523
Cahill <cpcahill@amazon.com>
2015-04-22 21:49:42 -0400

    Assignment 2 complete

--- cachematrix.R
+++ cachematrix.R
@@ -9,17 +9,94 @@
 ##                  solution and using that solution if the same operation
 ##                  is repeated (rather than solving it again).
 
-## Write a short comment describing this function
+##
+## Function:        makeCacheMatrix
+##
+## Parameters:      m - a matrix (which defaults to an empty matrix)
+##
+## Description:     Creates a cached inverse matrix structure used by cachematrix() to 
+##                  record the value of a solved matrix inversion so that it is avialble
+##                  for subsequent invocations
+##
+makeCacheMatrix <- function(m = matrix()) {
+    #
+    # local storage of the inversed matrix
+    #
+    inv <- NULL
 
+    #
+    # set(y) - operation to set the initial values of the cached matrix to y
+    #
+    set <- function(y) {
+        m <<- y
+        inv <<- NULL
+    }
 
-makeCacheMatrix <- function(x = matrix()) {
+    #
+    # get() -  operation to get the original matrix
+    #
+    get <- function() {
+        m
+    }
 
+    #
+    # setinverse(new_inv) - operation to set the inverse of the matrix to new_inv
+    #
+    setinverse <- function(new_inv) {
+        inv <<- new_inv
+    }
+
+    #
+    # getinverse() - operation to get the cached inverse of the matrix
+    #
+    getinverse <- function() {
+        inv
+    }
+
+    #
+    # return a vector that includes all of the operations we just defined
+    #
+    list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 
 }
 
+##
+## Function:        casheSolve()
+##
+## Parameters:      m - a cached matrix created using makeCacheMatrix()
+##                  ... - extra parameters for the solve() function
+##
+## Description:     Checks to see if we've already solved this matrix and
+##                  if so, returns the cached solution.  Otherwise solve 
+##                  the matrix, store the solution in the cache and return
+##                  the calculated value.
+##
+cacheSolve <- function(m, ...) {
 
-## Write a short comment describing this function
+    #
+    # check to see if we've already solved this matrix and if so,
+    # return the cached value
+    #
+    inv <- m$getinverse()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    #
+    # Ok, we didn't have the cached value, so we have to calculate 
+    # the value ourselves, so get the data and use solve to invert
+    # the matrix
+    #
+    data <- m$get()
+    inv <- solve(data, ...)
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    #
+    # store the results in the cache so we can use that next time
+    #
+    m$setinverse(inv)
+
+    #
+    # return the calculated inverse
+    #
+    inv
 }
--- testcache.R
+++ testcache.R
@@ -1,5 +1,65 @@
 #!/usr/bin/Rscript
-#
+##
+## Script to run tests against cacheSolve & makeCacheMatrix() for assignment 2
+##
 
-cat( "testing cache\n" )
+cat( "----------------------------------------------------------------------\n")
+cat( "-----> Testing cacheSolve()...\n")
+cat( "sourcing cachematrix.R...\n")
+source('cachematrix.R')
+
+cat( "----------------------------------------------------------------------\n")
+cat( "--> Test 1: 3x3 matrix...\n" )
+
+m1 = matrix( c(1, 1, 1, 3, 4, 3, 3, 3, 4), nrow=3, ncol=3)
+
+cat("Original matrix:\n")
+m1
+
+x1 = makeCacheMatrix(m1)
+
+y1 = cacheSolve(x1)
+
+cat("Inverted matrix:\n")
+y1
+
+cat("testing multiplication to get identity matrix:\n")
+m1 %*% y1
+
+cat( "----------------------------------------------------------------------\n")
+cat( "--> Test 2: same matrix, should get message about cache\n")
+y2 = cacheSolve(x1)
+
+y2
+
+
+cat( "----------------------------------------------------------------------\n")
+cat( "--> Test 3: 2x2 matrix\n");
+
+m3 = matrix( c(4, 3, 3, 2), nrow=2, ncol=2)
+cat("Original matrix:\n")
+m3
+
+x3 = makeCacheMatrix(m3)
+
+y3 = cacheSolve(x3)
+
+cat("Inverted matrix:\n")
+y3
+
+cat("testing multiplication to get identity matrix:\n")
+m3 %*% y3
+
+
+cat( "----------------------------------------------------------------------\n")
+cat( "--> Test 4: same matrix as test 3, should get message about cache\n")
+y4 = cacheSolve(x3)
+
+y4
+
+cat( "----------------------------------------------------------------------\n")
+cat( "--> Test 5: same matrix as test 1, should get message about cache\n")
+y5 = cacheSolve(x1)
+
+y5
 
